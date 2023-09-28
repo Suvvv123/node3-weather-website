dd7ca1ab862c8c711164d36fa190a944d48fb6ae
Suvvv123 <suvarnamanjari14@gmail.com>
2023-09-28 13:26:18 +0530

    setup app for cyclic

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location = search.value
     msgOne.textContent='Loading....'
     msgTwo.textContent=''
-    fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
+    fetch('/weather?address='+ location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 console.log('enter a valid search ')
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //define paths for express config
 const directPath = path.join(__dirname,'../public')
@@ -82,6 +83,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port '+port)
 })
